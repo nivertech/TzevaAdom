e9dadee6247006bf9ecbaed63b078e06b70b81ca
Dotan Simha <dotansimha@gmail.com>
2014-07-11 17:31:36 +0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tzevaAdomApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tzevaAdomApp
+ */
+angular.module('tzevaAdomApp')
+.controller('MainCtrl', ['$scope', '$http', '$interval', 'ngAudio', function ($scope, $http, $interval, ngAudio) {
+        $scope.alerts = [];
+        $scope.isLoading = false;
+        $scope.prevAlerts = [];
+
+        $interval(function () {
+            $scope.isLoading = true;
+            
+            $http.get('http://tzevadom.net84.net/').success(function (response) {
+                $scope.prevAlerts = $scope.prevAlerts.concat($scope.alerts);
+                $scope.alerts = angular.fromJson(response.replace(/\r?\n?[^\r\n]*$/, "")).data;
+                
+                var tmpArr = [];
+                
+                angular.forEach($scope.alerts, function(value) {
+                    tmpArr.push({place: value, time: new Date()});
+                });
+                
+                if ($scope.alerts.length > 0)
+                {
+                    ngAudio.play('http://soundjax.com/reddo/5954%5ERINGOUT.mp3');
+                }
+                
+                $scope.isLoading = false;
+            });
+        }, 5000);
+    }]);
